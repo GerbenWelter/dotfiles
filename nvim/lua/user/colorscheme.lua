b22d7969c8704c6b915f38e737b8ddeba7b04d8e
Gerben Welter <gerben.welter@hcs-company.com>
2023-01-18 13:29:33 +0100

    Add Neovim config

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+--local colorscheme = "codedark"
+--local colorscheme = "sonokai"
+--local colorscheme = "gruvbox"
+local colorscheme = "noctis_obscuro"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
