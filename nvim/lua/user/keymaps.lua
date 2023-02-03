7d8248c1fb1d9a0694f79cab542da686c9262a2e
Gerben Welter <gerben.welter@hcs-company.com>
2023-02-03 14:49:23 +0100

    Make NeoTree open with focus

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -6,7 +6,5 @@ local keymap = vim.api.nvim_set_keymap
 -- Keymaps
 keymap('n', '<F5>', ':set nonumber! number?<CR>', opts)
 keymap('n', '<F24>', ':PackerSync<CR>', opts)
-
---keymap('n', '<c-n>', ':NvimTreeToggle<CR>', opts)
-keymap('n', '<c-n>', ':NeoTreeShowToggle<CR>', opts)
+keymap('n', '<c-n>', ':NeoTreeFocusToggle<CR>', opts)
 
