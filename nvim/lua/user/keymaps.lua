5406557d7f7216c21d5bce18a05380a3e9b4d60a
Gerben Welter <gerben.welter@hcs-company.com>
2023-02-07 07:47:37 +0100

    Try using kickstart.nvim

--- nvim/lua/user/keymaps.lua
+++
@@ -1,9 +0,0 @@
-local opts = { noremap = true, silent = true }
-
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
--- Keymaps
-keymap("n", "<F5>", ":set nonumber! number?<CR>", opts)
-keymap("n", "<F24>", ":PackerSync<CR>", opts)
-keymap("n", "<c-n>", ":NeoTreeFocusToggle<CR>", opts)
