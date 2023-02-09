d50d121eb8d854aef4dc394005135224264d3eae
Gerben Welter <gerben.welter@hcs-company.com>
2023-02-09 11:31:45 +0100

    Add bufferline

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,6 +54,11 @@ require("packer").startup(function(use)
   use("navarasu/onedark.nvim") -- Theme inspired by Atom
   use("talha-akram/noctis.nvim")
 
+  use({ -- bufferline at top of screen
+    "akinsho/bufferline.nvim",
+    tag = "v3.*",
+    requires = "nvim-tree/nvim-web-devicons",
+  })
   use("nvim-lualine/lualine.nvim") -- Fancier statusline
   use("lukas-reineke/indent-blankline.nvim") -- Add indentation guides even on blank lines
   use("numToStr/Comment.nvim") -- "gc" to comment visual regions/lines
@@ -458,6 +463,51 @@ cmp.setup({
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
+  },
+})
+
+local status, bufferline = pcall(require, "bufferline")
+if not status then
+	print("ERROR bufferline")
+	return
+end
+
+bufferline.setup({
+  highlights = {
+    fill = {},
+    background = {},
+    tab_selected = {},
+    buffer_selected = { italic = false },
+    diagnostic_selected = { italic = false },
+    hint_selected = { italic = false },
+    pick_selected = { italic = false },
+    pick_visible = { italic = false },
+    pick = { italic = false },
+  },
+
+  options = {
+    indicator = {
+      icon = "▎",
+      style = "icon",
+    },
+    buffer_close_icon = "",
+    modified_icon = "●",
+    close_icon = "",
+    left_trunc_marker = "",
+    right_trunc_marker = "",
+    -- numbers = "ordinal",
+    max_name_length = 15,
+    max_prefix_length = 6,
+    diagnostics = "nvim_lsp",
+    show_buffer_icons = true,
+    show_buffer_close_icons = true,
+    show_close_icon = true,
+    persist_buffer_sort = true,
+    enforce_regular_tabs = true,
+    diagnostics_indicator = function(count, level)
+      local icon = level:match("error") and "" or ""
+      return icon .. count
+    end,
   },
 })
 
