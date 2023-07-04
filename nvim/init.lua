671859289eee9be05e1c50bd15cbedfae7ad1a1a
Gerben Welter <gerben@welter.nu>
2023-07-04 19:25:14 +0200

    Configure a second keymapping for PackerSync
    Some terminal register Shift F12 as <S-F12> while others register <F24>.

--- nvim/init.lua
+++ nvim/init.lua
@@ -212,6 +212,7 @@ vim.keymap.set("n", "<leader>z", ":set wrap!<CR>", { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "<S-F12>", ":PackerSync<CR>", { silent = true })
 vim.keymap.set("n", "<F24>", ":PackerSync<CR>", { silent = true })
 vim.keymap.set("n", "<c-n>", ":NeoTreeFocusToggle<CR>", { silent = true })
 
