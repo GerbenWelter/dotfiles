2f1fd159f6ae625aac46565500f4cf74e7799aef
Gerben Welter <gerben@welter.nu>
2023-10-05 16:45:07 +0200

    Disable the new scope option of IndentBlankLine

--- nvim/lua/gerben/plugins/misc.lua
+++ nvim/lua/gerben/plugins/misc.lua
@@ -1,6 +1,6 @@
 return {
 	{ "lewis6991/gitsigns.nvim", config = true },
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }, -- Add indentation guides even on blank lines
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = { scope = { enabled = false } } }, -- Add indentation guides even on blank lines
 	{ "numToStr/Comment.nvim", config = true }, -- "gc" to comment visual regions/lines
 	"ntpeters/vim-better-whitespace",
 	"tpope/vim-repeat",
