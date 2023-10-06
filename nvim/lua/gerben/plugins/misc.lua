1bd33ba4c75242fc048974fd8056a1b69bab4328
Gerben Welter <gerben@welter.nu>
2023-10-06 10:21:52 +0200

    Split of IndentBlankLine into own lua file and fix toggle

--- nvim/lua/gerben/plugins/misc.lua
+++ nvim/lua/gerben/plugins/misc.lua
@@ -1,6 +1,5 @@
 return {
 	{ "lewis6991/gitsigns.nvim", config = true },
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = { scope = { enabled = false } } }, -- Add indentation guides even on blank lines
 	{ "numToStr/Comment.nvim", config = true }, -- "gc" to comment visual regions/lines
 	"ntpeters/vim-better-whitespace",
 	"tpope/vim-repeat",
