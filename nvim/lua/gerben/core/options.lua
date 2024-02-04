0834c0b9c3fd8ba70ad704e8507af3fb61baaa39
Gerben Welter <gerben@welter.nu>
2024-02-04 19:39:45 +0100

    Remember last position in file

--- nvim/lua/gerben/core/options.lua
+++ nvim/lua/gerben/core/options.lua
@@ -54,3 +54,24 @@ vim.opt.sidescrolloff = 8
 
 -- Don't show keystrokes in CmdLine
 vim.opt.showcmd = false
+
+-- Remember last position in file
+vim.api.nvim_create_autocmd("BufRead", {
+	callback = function(opts)
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			once = true,
+			buffer = opts.buf,
+			callback = function()
+				local ft = vim.bo[opts.buf].filetype
+				local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+				if
+					not (ft:match("commit") and ft:match("rebase"))
+					and last_known_line > 1
+					and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+				then
+					vim.api.nvim_feedkeys([[g`"]], "nx", false)
+				end
+			end,
+		})
+	end,
+})
