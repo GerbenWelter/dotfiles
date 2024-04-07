74686e6257c87c2bc32862796db5158eac8c445d
Gerben Welter <gerben@welter.nu>
2024-04-07 13:25:42 +0200

    Rework keymaps

--- nvim/lua/gerben/core/keymaps.lua
+++ nvim/lua/gerben/core/keymaps.lua
@@ -1,10 +1,6 @@
 -- Map key for toggling word wrap
 vim.keymap.set("n", "<leader>z", ":set wrap!<CR>", { silent = true })
 
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 -- Create function and keymaps for toggling all columns
 -- for easier copy/paste.
 function _G.toggle_columns()
@@ -25,3 +21,15 @@ vim.keymap.set("n", "<C-k>", ":BufferLineCycleNext<CR>")
 
 vim.keymap.set("n", "<S-C-j>", ":BufferLineMovePrev<CR>")
 vim.keymap.set("n", "<S-C-k>", ":BufferLineMoveNext<CR>")
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
