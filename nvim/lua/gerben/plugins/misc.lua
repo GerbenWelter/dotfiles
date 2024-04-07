2a66d28102e45acf09236e56de98cae3c0d4a30f
Gerben Welter <gerben@welter.nu>
2024-04-07 13:11:35 +0200

    Expand use of gitsigns

--- nvim/lua/gerben/plugins/misc.lua
+++ nvim/lua/gerben/plugins/misc.lua
@@ -1,5 +1,4 @@
 return {
-	{ "lewis6991/gitsigns.nvim", config = true },
 	{ "numToStr/Comment.nvim", config = true }, -- "gc" to comment visual regions/lines
 	"ntpeters/vim-better-whitespace",
 	"tpope/vim-repeat",
