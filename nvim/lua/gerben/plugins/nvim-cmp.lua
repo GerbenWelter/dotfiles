153cf941b9feab963c4f6b702089199dd49cbdb4
Gerben Welter <gerben@welter.nu>
2024-04-07 13:41:07 +0200

    Rework nvim-cmp

--- nvim/lua/gerben/plugins/nvim-cmp.lua
+++ nvim/lua/gerben/plugins/nvim-cmp.lua
@@ -5,23 +5,26 @@ return {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
 		"onsails/lspkind.nvim",
-		"L3MON4D3/LuaSnip", -- snippet engine
+		{
+			"L3MON4D3/LuaSnip", -- snippet engine
+			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number
+		},
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 	},
 	config = function()
 		local cmp = require("cmp")
-
 		local luasnip = require("luasnip")
-
 		local lspkind = require("lspkind")
 
-		-- load vs-code like snippets from plugins (e.g. friendly-snippets)
+		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 
-		vim.opt.completeopt = "menu,menuone,preview,noselect"
 		cmp.setup({
-			snippet = {
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
@@ -33,16 +36,17 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				{ name = "nvim_lsp" }, -- lsp
+				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
 			}),
-			-- configure lspkind for vs-code like icons
+
+			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
 				format = lspkind.cmp_format({
 					maxwidth = 50,
