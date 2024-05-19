c71e24bbc2cce40487c4c9ecbd16810ea3b09f4d
Gerben Welter <gerben@welter.nu>
2024-05-19 11:38:10 +0200

    Configure lsp inlay hints

--- nvim/lua/gerben/core/keymaps.lua
+++ nvim/lua/gerben/core/keymaps.lua
@@ -33,3 +33,10 @@ vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current t
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- toggle inlay hints
+if vim.lsp.inlay_hint then
+	vim.keymap.set("n", "<leader>uh", function()
+		vim.lsp.inlay_hint(0, nil)
+	end, { desc = "Toggle Inlay Hints" })
+end
