c7118c493354bebf23d0b79f405728dd09d9559c
Gerben Welter <gerben@welter.nu>
2024-06-15 16:13:27 +0200

    Fix yaml queries

--- dot-config/nvim/queries/yaml/injections.scm
+++ dot-config/nvim/queries/yaml/injections.scm
@@ -5,4 +5,23 @@
 	value: (block_node
 			 (block_scalar) @injection.content
 			 (#set! injection.language "gotmpl"))
-	(#offset! @injection.content 0 1 0 0)))
+	(#offset! @injection.content 1 -22 0 0)
+	))
+
+;; ConfigMaps with *.json files
+(document
+  (block_node
+	(block_mapping
+	  (block_mapping_pair
+		key: (flow_node) @_data (#eq? @_data "data")
+		value: (block_node
+				 (block_mapping
+				   (block_mapping_pair
+					 key: (flow_node) @_json_file
+					 (#eq? @_json_file "config.json")
+					 value: (block_node
+							  (block_scalar) @injection.content
+							  (#set! injection.language "json")
+							  ; block_scalar must use offset which is relative to the '|'
+							  (#offset! @injection.content 1 -15 0 0)
+					 ))))))))
