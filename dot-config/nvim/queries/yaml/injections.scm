d90441dec6c72123d3a903816940dc4b5a0e723b
Gerben Welter <gerben@welter.nu>
2024-06-16 15:55:14 +0200

    Simpify gotmpl detection

--- dot-config/nvim/queries/yaml/injections.scm
+++ dot-config/nvim/queries/yaml/injections.scm
@@ -9,19 +9,28 @@
 	))
 
 ;; ConfigMaps with *.json files
-(document
-  (block_node
-	(block_mapping
-	  (block_mapping_pair
-		key: (flow_node) @_data (#eq? @_data "data")
-		value: (block_node
-				 (block_mapping
-				   (block_mapping_pair
-					 key: (flow_node) @_json_file
-					 (#eq? @_json_file "config.json")
-					 value: (block_node
-							  (block_scalar) @injection.content
-							  (#set! injection.language "json")
-							  ; block_scalar must use offset which is relative to the '|'
-							  (#offset! @injection.content 1 -15 0 0)
-					 ))))))))
+; (document
+;   (block_node
+; 	(block_mapping
+; 	  (block_mapping_pair
+; 		key: (flow_node) @_data (#eq? @_data "data")
+; 		value: (block_node
+; 				 (block_mapping
+; 				   (block_mapping_pair
+; 					 key: (flow_node) @_json_file
+; 					 (#eq? @_json_file "config.json")
+; 					 value: (block_node
+; 							  (block_scalar) @injection.content
+; 							  (#set! injection.language "json")
+; 							  ; block_scalar must use offset which is relative to the '|'
+; 							  (#offset! @injection.content 1 -15 0 0)
+; 					 ))))))))
+
+(block_mapping_pair
+  value: [
+    (block_node (block_scalar) @injection.content)
+    (flow_node (single_quote_scalar) @injection.content)
+  ]
+  (#set! injection.language "gotmpl")
+  (#contains? @injection.content "{{")
+)
