ee69b457feef7fc8ca8f27b52745d122f3d03f1a
Gerben Welter <gerben@welter.nu>
2025-03-15 23:26:58 +0100

    Add lsp_status

--- dot-config/nvim/lua/gerben/plugins/lualine.lua
+++ dot-config/nvim/lua/gerben/plugins/lualine.lua
@@ -55,6 +55,22 @@ return {
 				theme = my_lualine_theme,
 			},
 			sections = {
+				lualine_a = {
+					{
+						"lsp_status",
+						icon = "", -- f013
+						symbols = {
+							-- Standard unicode symbols to cycle through for LSP progress:
+							spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+							-- Standard unicode symbol for when LSP is done:
+							done = "✓",
+							-- Delimiter inserted between LSP names:
+							separator = " ",
+						},
+						-- List of LSP names to ignore (e.g., `null-ls`):
+						ignore_lsp = {},
+					},
+				},
 				lualine_x = {
 					{
 						lazy_status.updates,
