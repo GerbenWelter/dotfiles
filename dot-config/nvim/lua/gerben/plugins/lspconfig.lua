64a70f52981e6a63b4f8b3a406735dc618ee7154
Gerben Welter <gerben@welter.nu>
2025-03-15 23:26:29 +0100

    Use rust-analyzer from flatpak SDK if it exists

--- dot-config/nvim/lua/gerben/plugins/lspconfig.lua
+++ dot-config/nvim/lua/gerben/plugins/lspconfig.lua
@@ -167,6 +167,11 @@ return {
 							},
 						},
 					})
+					if vim.fn.filereadable("/lib/sdk/rust-stable/bin/rust-analyzer") == 1 then
+						lspconfig["rust_analyzer"].setup({
+							cmd = { "/lib/sdk/rust-stable/bin/rust-analyzer" },
+						})
+					end
 				end
 			end,
 		})
