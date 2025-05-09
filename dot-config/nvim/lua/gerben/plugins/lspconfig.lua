75ae5a92b4dc73827bdf76e841a301c57db6eed1
Gerben Welter <gerben@welter.nu>
2025-05-09 16:35:02 +0200

    Cleanup LSP configuration

--- dot-config/nvim/lua/gerben/plugins/lspconfig.lua
+++ dot-config/nvim/lua/gerben/plugins/lspconfig.lua
@@ -1,123 +1,41 @@
 return {
-	"hrsh7th/cmp-nvim-lsp",
-	config = function(ev)
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local opts = { buffer = ev.buf, silent = true }
+	"neovim/nvim-lspconfig",
+	config = function()
+		local config = {
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+			update_in_insert = true,
+			underline = true,
+			severity_sort = true,
+			float = {
+				source = true,
+				header = "",
+				prefix = "",
+				suffix = "",
+			},
+		}
+		vim.diagnostic.config(config)
 
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
-		-- set keybinds
-		opts.desc = "Show LSP references"
-		vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-		opts.desc = "Go to declaration"
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-		opts.desc = "Show LSP definitions"
-		vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-		opts.desc = "Show LSP implementations"
-		vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-		opts.desc = "Show LSP type definitions"
-		vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-		opts.desc = "See available code actions"
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-		opts.desc = "Smart rename"
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-		opts.desc = "Show buffer diagnostics"
-		vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-		opts.desc = "Show line diagnostics"
-		vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-		vim.diagnostic.config({ jump = { float = true } })
-
-		opts.desc = "Go to previous diagnostic"
-		vim.keymap.set("n", "[d", function()
-			vim.diagnostic.jump({ count = -1 })
-		end, opts) -- jump to previous diagnostic in buffer
-
-		opts.desc = "Go to next diagnostic"
-		vim.keymap.set("n", "]d", function()
-			vim.diagnostic.jump({ count = 1 })
-		end, opts) -- jump to next diagnostic in buffer
-
-		opts.desc = "Show documentation for what is under cursor"
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-		opts.desc = "Restart LSP"
-		vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-
-		vim.lsp.config("luals", {
-			on_init = function(client)
-				if client.workspace_folders then
-					local path = client.workspace_folders[1].name
-					if
-						path ~= vim.fn.stdpath("config")
-						and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-					then
-						return
-					end
-				end
-
-				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-					runtime = {
-						-- Tell the language server which version of Lua you're using (most
-						-- likely LuaJIT in the case of Neovim)
-						version = "LuaJIT",
-						-- Tell the language server how to find Lua modules same way as Neovim
-						-- (see `:h lua-module-load`)
-						path = {
-							"lua/?.lua",
-							"lua/?/init.lua",
-						},
-					},
-					-- Make the server aware of Neovim runtime files
-					workspace = {
-						checkThirdParty = false,
-						library = {
-							vim.env.VIMRUNTIME,
-							-- Depending on the usage, you might want to add additional paths
-							-- here.
-							"${3rd}/luv/library",
-							-- '${3rd}/busted/library'
-						},
-						-- Or pull in all of 'runtimepath'.
-						-- NOTE: this is a lot slower and will cause issues when working on
-						-- your own configuration.
-						-- See https://github.com/neovim/nvim-lspconfig/issues/3189
-						-- library = {
-						--   vim.api.nvim_get_runtime_file('', true),
-						-- }
-					},
-				})
-			end,
+		vim.lsp.config("*", {})
+		vim.lsp.config("lua_ls", {
 			settings = {
-				Lua = {},
+				Lua = {
+					workspace = {
+						library = { vim.env.VIMRUNTIME, "${3rd}/luv/library" },
+					},
+				},
 			},
 		})
 
 		if os.getenv("NVIM_RUST") then
-			require("lspconfig").rust_analyzer.setup({
-				capabilities = capabilities,
-				settings = {
-					["rust-analyzer"] = {
-						diagnostics = {
-							enable = false,
-						},
-					},
-				},
-			})
 			if vim.fn.filereadable("/lib/sdk/rust-stable/bin/rust-analyzer") == 1 then
-				require("lspconfig").rust_analyzer.setup({
+				vim.lsp.config("rust_analyzer", {
 					cmd = { "/lib/sdk/rust-stable/bin/rust-analyzer" },
 				})
 			end
