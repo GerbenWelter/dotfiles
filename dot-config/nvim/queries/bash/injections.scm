1b4c97d38686dd7888f35c3c9cfe57b0630051c1
Gerben Welter <gerben@welter.nu>
2025-10-17 21:45:36 +0200

    Format bash injections.scm

--- dot-config/nvim/queries/bash/injections.scm
+++ dot-config/nvim/queries/bash/injections.scm
@@ -1,10 +1,10 @@
 ;; Case: something | jq '.foo'
 (pipeline
   (command
-	name: (command_name) @cmd (#eq? @cmd "jq")
-	argument: (raw_string) @injection.content
-	(#offset! @injection.content 0 1 0 -1)
-	(#set! injection.language "jq")
-	)
+    name: (command_name) @cmd
+    (#eq? @cmd "jq")
+    argument: (raw_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "jq")
   )
-
+)
