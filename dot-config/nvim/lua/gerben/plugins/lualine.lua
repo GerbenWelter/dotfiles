bccf8841423f5c7ead679c01e954e4d6eb0c157c
Gerben Welter <gerben@welter.nu>
2025-10-17 21:52:21 +0200

    Add debugging plugin

--- dot-config/nvim/lua/gerben/plugins/lualine.lua
+++ dot-config/nvim/lua/gerben/plugins/lualine.lua
@@ -53,6 +53,14 @@ return {
 		lualine.setup({
 			options = {
 				theme = my_lualine_theme,
+				ignore_focus = {
+					"dapui_watches",
+					"dapui_breakpoints",
+					"dapui_scopes",
+					"dapui_console",
+					"dapui_stacks",
+					"dap-repl",
+				},
 			},
 			sections = {
 				lualine_a = {
