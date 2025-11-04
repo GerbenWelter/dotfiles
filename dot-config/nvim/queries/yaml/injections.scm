3b88159fcb8a1d826ca2491248271f1484a6c06f
Gerben Welter <gerben@welter.nu>
2025-11-04 11:17:30 +0100

    Add comment based injection for yaml sequences

--- dot-config/nvim/queries/yaml/injections.scm
+++ dot-config/nvim/queries/yaml/injections.scm
@@ -19,3 +19,28 @@
   (#offset! @injection.language 0 2 0 0)
   (#offset! @injection.content 1 2 0 0)
 )
+
+; keys:
+;   - | # json
+;     {
+;       "foo": "bar"
+;     }
+(block_mapping
+  (block_mapping_pair
+    key: (flow_node)
+    value: (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_scalar
+              (comment) @injection.language
+            ) @injection.content
+          )
+        )
+      )
+    )
+  )
+  (#set! injection.include-children)
+  (#offset! @injection.language 0 2 0 0)
+  (#offset! @injection.content 1 -2 0 0)
+)
