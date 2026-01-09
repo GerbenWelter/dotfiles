d0702f48f2a34c3830a09fdf0373885b5c309025
Gerben Welter <ga.welter@belastingdienst.nl>
2026-01-09 15:17:52 +0100

    Add Markview config

--- dot-config/nvim/lua/gerben/plugins/markview.lua
+++ dot-config/nvim/lua/gerben/plugins/markview.lua
@@ -1,4 +1,14 @@
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
+
+	config = function()
+		local markview = require("markview")
+
+		markview.setup({
+			preview = { enable = false },
+		})
+
+		vim.keymap.set("n", "<leader>mvs", ":Markview splitToggle<CR>", { desc = "Toggle Markview split view" })
+	end,
 }
