17f1e0adafcee306addfcd5298138e631fea7489
Gerben Welter <gerben@welter.nu>
2026-03-30 11:59:56 +0200

    Fix rust-analyzer settings workaround for neovim 0.12

--- dot-config/nvim/lsp/rust_analyzer.lua
+++ dot-config/nvim/lsp/rust_analyzer.lua
@@ -1,7 +1,12 @@
+-- Workaround for using rust-analyzer on Fedora Silverblue
 if vim.fn.filereadable("/lib/sdk/rust-stable/bin/rust-analyzer") == 1 then
 	return {
-		vim.lsp.config("rust_analyzer", {
-			cmd = { "/lib/sdk/rust-stable/bin/rust-analyzer" },
-		}),
+		settings = {
+			["rust-analyzer"] = {
+				cmd = { "/lib/sdk/rust-stable/bin/rust-analyzer" },
+			},
+		},
 	}
+else
+	return {}
 end
